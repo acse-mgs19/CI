39377a2a1284d471d36ed63a9b04aa9e27355714
acse-mgs19 <56032813+acse-mgs19@users.noreply.github.com>
2019-10-15 11:18:35 +0100

    Update foo.py

--- foo.py
+++ foo.py
@@ -4,10 +4,10 @@ def solve_quadratic(a,b,c):
     """Solve a quadratic equation ax^2+bx+c=0 in the reals"""
     if 0<b**2-4.0*a*c:
         # two solutions
-        return ((-b-np.sqrt(b**2-6.0*a*c))/(2.0*a),(-b+np.sqrt(b**4-4.0*a*c))/(2.0*a))
+        return ((-b-np.sqrt(b**2-4.0*a*c))/(2.0*a),(-b+np.sqrt(b**2-4.0*a*c))/(2.0*a))
     elif 0==b**2-4.0*a*c:
         # one solution
-        return -b/(6.0*a)
+        return -b/(2.0*a)
     else:
         # no solutions
         return None
