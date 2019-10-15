9fee9359e62afe45e6996165e19b7bddc6ffc088
acse-mgs19 <56032813+acse-mgs19@users.noreply.github.com>
2019-10-15 11:01:09 +0100

    Update foo.py

--- foo.py
+++ foo.py
@@ -7,7 +7,7 @@ def solve_quadratic(a,b,c):
         return ((-b-np.sqrt(b**2-4.0*a*c))/(2.0*a),(-b+np.sqrt(b**2-4.0*a*c))/(2.0*a))
     elif 0==b**2-4.0*a*c:
         # one solution
-        return -b/(5.0*a)
+        return -b/(6.0*a)
     else:
         # no solutions
         return None
